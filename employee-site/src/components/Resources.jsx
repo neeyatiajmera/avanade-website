8854f204ca169f8138d35574127e43b223c7e152
Neeyati Ajmera <neeyatiajmera@gmail.com>
2020-08-13 13:36:58 -0700

    Updated resources tab

--- employee-site/src/components/Resources.jsx
+++ employee-site/src/components/Resources.jsx
@@ -7,7 +7,33 @@ function Resources() {
       <div class="row align-items-center">
       <div class="col-lg-12">
             <h1 class="font-weight-light">Resources</h1>
-            <p>Resources from CDC website, Avanade guidelines, testing centers nearby etc. </p>
+            <h2 class="font-weight-light">1.Return to Work after illness or exposure</h2>
+            <ul>
+        <li><b>Follow CDC guidance</b> for discontinuing
+self-isolation and returning to work after
+illness, or discontinuing self-quarantine
+and monitoring after exposure, as
+appropriate for the workplace.
+</li>
+      <li>Ensure workers who have been
+exposed to someone with COVID-19
+routinely monitor themselves or receive
+monitoring, including for signs and/or
+symptoms of potential illness, at work,
+in accordance with CDC guidance.</li>
+      </ul>
+
+      <h2 class="font-weight-light">2.Safe Practices at Work</h2>
+            <ul>
+        <li>Regular and thorough hand-washing with a soap by employees, employers and customers </li>
+      <li>Wear a cloth mask at all times whilst in vicinity of any other person. Maintain atleast 6ft of distance between you and the person.</li>
+      </ul>
+
+      <h2 class="font-weight-light">3.What to do if you're feeling sick</h2>
+            <ul>
+        <li>Self isolate and match your symptoms against symptoms on the CDC Website.</li>
+      <li>Get tested at a nearby testing center. Make sure to call before showing up to the center.</li>
+      </ul>
       
       </div>
       </div>
