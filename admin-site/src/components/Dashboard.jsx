7cb010bf4878342b001852210f7684db6d13c853
Shannon Trinh <shannon.trinh@avanade.com>
2020-08-18 10:15:04 -0700

    Refined dashboards and populated PPE tab

--- admin-site/src/components/Dashboard.jsx
+++ admin-site/src/components/Dashboard.jsx
@@ -68,11 +68,12 @@ function Dashboard() {
               </Tabs>
             </AppBar>
             <TabPanel value={value} index={0}>
-              <iframe src = "https://app.powerbi.com/reportEmbed?reportId=9b5704da-4cc4-41f1-8281-2c0262a190f8&groupId=9606f938-1f8a-4601-bd8e-660039edae4e&autoAuth=true&ctid=cf36141c-ddd7-45a7-b073-111f66d0b30c&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" width="950" height="600"> Loading... </iframe>
+              <iframe src = "https://app.powerbi.com/reportEmbed?reportId=f0dac6b4-d7cc-4f6c-9cc9-0c0ede2a23f4&groupId=9606f938-1f8a-4601-bd8e-660039edae4e&autoAuth=true&ctid=cf36141c-ddd7-45a7-b073-111f66d0b30c&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" width="950" height="600"> Loading... </iframe>
             </TabPanel>
             <TabPanel value={value} index={1}>
-              <iframe src = "https://app.powerbi.com/reportEmbed?reportId=9b5704da-4cc4-41f1-8281-2c0262a190f8&groupId=9606f938-1f8a-4601-bd8e-660039edae4e&autoAuth=true&ctid=cf36141c-ddd7-45a7-b073-111f66d0b30c&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" width="950" height="600"> Loading... </iframe>
+              <iframe src = "https://app.powerbi.com/reportEmbed?reportId=f0dac6b4-d7cc-4f6c-9cc9-0c0ede2a23f4&groupId=9606f938-1f8a-4601-bd8e-660039edae4e&autoAuth=true&ctid=cf36141c-ddd7-45a7-b073-111f66d0b30c&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" width="950" height="600"> Loading... </iframe>
             </TabPanel>
+            
       </div>
       </div>
       </div>
--- admin-site/src/components/PPE.jsx
+++ admin-site/src/components/PPE.jsx
@@ -11,7 +11,7 @@ function PPE() {
       </div>
       </div>
         <div class="row align-items-center my-5">
-        
+          <iframe src = "https://app.powerbi.com/reportEmbed?reportId=f0dac6b4-d7cc-4f6c-9cc9-0c0ede2a23f4&groupId=9606f938-1f8a-4601-bd8e-660039edae4e&autoAuth=true&ctid=cf36141c-ddd7-45a7-b073-111f66d0b30c&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" width="950" height="600"> Loading... </iframe>
         </div>
       </div>
     </div>
